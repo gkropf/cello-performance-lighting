3b4b656e70462951c6cb4eab5b441d117b1e4a78
Gabriel Kropf <gabriel.kropf@student.nmt.edu>
2020-10-11 12:48:42 -0600

    added user sslection for display

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from multiprocessing import Process, Queue, Manager
 from queue import Empty
 from scipy.io.wavfile import write
 from audio_processing import *
-#from GUI_display import *
-from LED_display import *
 
 
 # Set up queues for managing tasks.
@@ -11,13 +9,23 @@ audio_stream_queue = Queue()
 processed_audio_data_queue = Queue()
 final_audio = Queue()
 manager = Manager()
-program_state = manager.Value('int',1)
+program_state = manager.Value('int',4)
 
-# Start processes
+# Ask user which display method they would like to use
+method = ''
+while method not in ['LED','GUI']:
+	method = input('Which display method would you like to use (LED/GUI)?\n')
+
+# Start processes based on which display method was chosen
 process1 = Process(target=record_audio_thread, args=[audio_stream_queue,program_state])
 process2 = Process(target=audio_processing_thread, args=[audio_stream_queue,processed_audio_data_queue,final_audio])
-#process3 = Process(target=GUI_display_thread, args=[processed_audio_data_queue])
-process3 = Process(target=LED_display_thread, args=[processed_audio_data_queue])
+if method == 'GUI':
+	from GUI_display import *
+	process3 = Process(target=GUI_display_thread, args=[processed_audio_data_queue])
+elif method == 'LED':
+	from LED_display import *
+	process3 = Process(target=LED_display_thread, args=[processed_audio_data_queue])
+
 process1.start()
 process2.start()
 process3.start()
